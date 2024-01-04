45f40d3a41dff20d061cce25a34e69f8e4d7be28
yan34566 <155184373+yan34566@users.noreply.github.com>
2024-01-04 17:26:37 +0200

    друге завдання

--- main.py
+++ main.py
@@ -1,46 +1,25 @@
-print("Hello world")
-print()
-print('test')
+def meters_to_miles(meters):
+    return meters * 0.000621371
+def meters_to_inches(meters):
+    return meters * 39.370078740157
+def meters_to_yards(meters):
+    return meters * 1.09361329834
 
-# '''
-# однрядковий
-# багато рядковий
-# коментар
-# тут можно писати будьякий текст
+meters = float(input("введіть метри:"))
+choice = input("Обери одиниці для переведення (милі, дюйми, ярди): ")
 
+if choice == "милі":
+    result = meters_to_miles(meters)
+    print(f"{meters} метрів дорівнюe {result} миль")
 
+elif choice == "дюйми":
+    result = meters_to_inches(meters)
+    print(f"{meters} метрів дорівнюe {result} дюймів")
 
-number = int(input("Enter 3-digit number: "))
+elif choice == "ярди":
+    result = meters_to_yards(meters)
+    print(f"{meters} метрів дорівнюe {result} ярди")
 
-n1 = number // 100
+else:
+    print("неірний вибір одинці")
 
-n2 = number // 10 % 10
-
-
-n2 = number % 100 // 10
-n3 = number % 10
-
-result = n1 + n2 + n3
-print(f"n1: {n1} n2: {n2} n3 {n3}")
-print(f"Result: {result}")
-
-number = int(input("Enter 4-digit number: "))
-
-n1 = number // 100
-
-n2 = number // 10 % 10
-
-n2 = number % 100 // 10
-n3 = number % 10
-n4 = number % 10
-
-result = n1 + n2 + n3 + n4
-print(f"n1: {n1} n2: {n2} n3: {n3} n:4 {n4}  ")
-print(f"Result: {result}")
-
-diagonal1 = int(input("user input diagonal1"))
-diagonal2 = int(input("user input diagonal2"))
-
-area = (diagonal1 * diagonal2) / 2
-
-print(f"area: {area}")
