0c0658ed48e29a5965673d750dc0b47a43d654d9
yan34566 <155184373+yan34566@users.noreply.github.com>
2024-01-30 21:35:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,84 @@
-print("Hello world")
-print()
-print('test')
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+#
+# result = factorial(5)
+# print(result)
 
-# '''
-# однрядковий
-# багато рядковий
-# коментар
-# тут можно писати будьякий текст
+# def power(n, e):
+#     if e == 0:
+#         return 1
+#     elif e > 0:
+#         return n * power(n, e - 1)
+#     else:
+#         return 1 / (n * power(n, -e - 1))
+#
+# result_positiv = power(6, 2)
+# result_negativ = power(6, -2)
+# print(result_positiv)
+# print(result_negativ)
 
 
+# def stars(N):
+#     if N > 0:
+#         print('*', end='')
+#         stars(N - 1)
+#     else:
+#         print()
+#
+# user_input = int(input("введіть  кількість потрібних зірок: "))
+# stars(user_input)
 
-number = int(input("Enter 3-digit number: "))
+# def sum_range(a, b):
+#     if a > b:
+#         return 0
+#     else:
+#         return a + sum_range(a + 1, b)
+#
+# a = 1
+# b = 5
+# print(f"сума чисел в діапазоні від {a} до {b} дорівнює {sum_range(a, b)}")
 
-n1 = number // 100
+# def list:
+#     if a > b:
+#         return 0
+#     print(list)
+#     else:
+#         return max
 
-n2 = number // 10 % 10
-
-
-n2 = number % 100 // 10
-n3 = number % 10
-
-result = n1 + n2 + n3
-print(f"n1: {n1} n2: {n2} n3 {n3}")
-print(f"Result: {result}")
-
-number = int(input("Enter 4-digit number: "))
-
-n1 = number // 100
-
-n2 = number // 10 % 10
-
-n2 = number % 100 // 10
-n3 = number % 10
-n4 = number % 10
-
-result = n1 + n2 + n3 + n4
-print(f"n1: {n1} n2: {n2} n3: {n3} n:4 {n4}  ")
-print(f"Result: {result}")
-
-diagonal1 = int(input("user input diagonal1"))
-diagonal2 = int(input("user input diagonal2"))
-
-area = (diagonal1 * diagonal2) / 2
-
-print(f"area: {area}")
+# import random
+#
+#
+# def find_min_sequence_start(arr, start_index=0):
+#
+#
+#     if start_index + 10 > len(arr):
+#         return start_index
+#
+#
+#     current_sequence = arr[start_index:start_index + 10]
+#     current_sum = sum(current_sequence)
+#
+#
+#     next_start_index = find_min_sequence_start(arr, start_index + 1)
+#
+#
+#     next_sequence = arr[next_start_index:next_start_index + 10]
+#     next_sum = sum(next_sequence)
+#
+#     return start_index if current_sum < next_sum else next_start_index
+#
+# random_numbers = [random.randint(1, 100) for _ in range(100)]
+#
+#
+# start_position = find_min_sequence_start(random_numbers)
+#
+#
+# print("Список із рандомними числами: ", random_numbers)
+# print("Початкова позиція послідовності:", start_position)
+# min_sequence = random_numbers[start_position:start_position + 10]
+# min_sequence_sum = sum(min_sequence)
+# print("Мінімальна послідовність:", min_sequence)
+# print("Сума мінімальної послідовності:", min_sequence_sum)
