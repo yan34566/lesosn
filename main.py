d023afa6d1c45c8c2f113aee73c1f6da33b3c86c
yan34566 <155184373+yan34566@users.noreply.github.com>
2024-02-06 22:07:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,84 +1,62 @@
-# def factorial(n):
-#     if n == 0 or n == 1:
-#         return 1
-#     else:
-#         return n * factorial(n-1)
-#
-# result = factorial(5)
-# print(result)
+import re
 
-# def power(n, e):
-#     if e == 0:
-#         return 1
-#     elif e > 0:
-#         return n * power(n, e - 1)
-#     else:
-#         return 1 / (n * power(n, -e - 1))
+# def home_number(phone_nuber):
+#     pattern = r'^\d{5,7}$'
+#     return bool(re.match(pattern, phone_nuber))
 #
-# result_positiv = power(6, 2)
-# result_negativ = power(6, -2)
-# print(result_positiv)
-# print(result_negativ)
+# user_input = input('ведить номер домшнего телефону: ')
+#
+# result =  home_number(user_input)
+# if result:
+#     print('номер валідний')
+# else:
+#     print('номер не валідний')
 
+# def mobali_number(phone_nuber):
+#     pattern = r'^(\+\d{1,3})?\d{9,10}$'
+#     return bool(re.match(pattern, phone_nuber))
+#
+# user_input = input('ведить номер мобільного телефону: ')
+#
+# result = mobali_number(user_input)
+# if result:
+#     print('номер мобільний валідний')
+# else:
+#     print('номер мобільній не валідний')
 
-# def stars(N):
-#     if N > 0:
-#         print('*', end='')
-#         stars(N - 1)
-#     else:
-#         print()
+# def Email_address(email):
+#     pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,20}$'
+#     return bool(re.match(pattern, email))
 #
-# user_input = int(input("введіть  кількість потрібних зірок: "))
-# stars(user_input)
+# user_input = input('ведіть інтернет адресу: ')
+#
+# result = Email_address(user_input)
+# if result:
+#     print('адреса валідна')
+# else:
+#     print('адреса не валідна')
 
-# def sum_range(a, b):
-#     if a > b:
-#         return 0
-#     else:
-#         return a + sum_range(a + 1, b)
+# def full_name(name):
+#     pattern = r'^[a-zA-Zа-яА-Я]{2,20}( [a-zA-Zа-яА-Я]{2,20}){2}$'
+#     return bool(re.match(pattern, name))
 #
-# a = 1
-# b = 5
-# print(f"сума чисел в діапазоні від {a} до {b} дорівнює {sum_range(a, b)}")
+# user_input = input('ведііть повне ім я: ')
+#
+# result = full_name(user_input)
+# if result:
+#     print('імя валідне')
+# else:
+#     print('імя не валідне')
 
-# def list:
-#     if a > b:
-#         return 0
-#     print(list)
-#     else:
-#         return max
+# def creat_pasword(pasword):
+#     pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$'
+#     return bool(re.match(pattern, pasword))
+#
+# user_input = input('ведіть повне ім я: ')
+#
+# result = creat_pasword(user_input)
+# if result:
+#     print('пароль валідний')
+# else:
+#     print('пароль не валідний')
 
-# import random
-#
-#
-# def find_min_sequence_start(arr, start_index=0):
-#
-#
-#     if start_index + 10 > len(arr):
-#         return start_index
-#
-#
-#     current_sequence = arr[start_index:start_index + 10]
-#     current_sum = sum(current_sequence)
-#
-#
-#     next_start_index = find_min_sequence_start(arr, start_index + 1)
-#
-#
-#     next_sequence = arr[next_start_index:next_start_index + 10]
-#     next_sum = sum(next_sequence)
-#
-#     return start_index if current_sum < next_sum else next_start_index
-#
-# random_numbers = [random.randint(1, 100) for _ in range(100)]
-#
-#
-# start_position = find_min_sequence_start(random_numbers)
-#
-#
-# print("Список із рандомними числами: ", random_numbers)
-# print("Початкова позиція послідовності:", start_position)
-# min_sequence = random_numbers[start_position:start_position + 10]
-# min_sequence_sum = sum(min_sequence)
-# print("Мінімальна послідовність:", min_sequence)
-# print("Сума мінімальної послідовності:", min_sequence_sum)
